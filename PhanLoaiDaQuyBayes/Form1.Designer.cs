810da279208e5cc151cf31178189f3622ebb1eb7
Dinhvuquang <dinhvuquang264@gmail.com>
2024-06-01 23:01:21 +0700

    first commit

---
+++ PhanLoaiDaQuyBayes/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PhanLoaiDaQuyBayes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lbKDD = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chọn File huấn luyện";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 84);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Bắt đầu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(231, 422);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(1, 178);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lbKDD);
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Size = new System.Drawing.Size(685, 422);
+            this.splitContainer1.SplitterDistance = 231;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // lbKDD
+            // 
+            this.lbKDD.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbKDD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKDD.Location = new System.Drawing.Point(0, 235);
+            this.lbKDD.Name = "lbKDD";
+            this.lbKDD.Size = new System.Drawing.Size(450, 187);
+            this.lbKDD.TabIndex = 3;
+            this.lbKDD.Text = "Loại đá quý được dự đoán: ";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(450, 200);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(689, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Phân Loại Đá Quý - Thuật Toán Naive Bayes";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lbKDD;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PhanLoaiDaQuyBayes/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PhanLoaiDaQuyBayes
+{
+    public partial class Form1 : Form
+    {
+        struct PredictionClass
+        {
+            public string name;
+            public int count;
+            public double p;
+        }
+
+        struct DataTrain
+        {
+            public string name;
+            public int[] count;
+            public double[] p;
+            public int column;
+        }
+
+        PredictionClass[] predictionClass;
+        DataTrain[] dataTrain;
+        int totalPredictionClass;
+        int totalDataTrain;
+
+        public Form1()
+        {
+            InitializeComponent();
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            flowLayoutPanel1.Dock = DockStyle.Top;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            predictionClass = new PredictionClass[100];
+            dataTrain = new DataTrain[100];
+            totalPredictionClass = 1;
+            totalDataTrain = 0;
+
+            richTextBox1.Clear();
+            dataGridView1.Refresh();
+
+            while (flowLayoutPanel1.Controls.Count > 0)
+            {
+                flowLayoutPanel1.Controls[flowLayoutPanel1.Controls.Count - 1].Dispose();
+            }
+            // Mở hộp thoại chọn file
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Đọc dữ liệu từ tệp CSV
+                using (var reader = new StreamReader(openFileDialog1.FileName))
+                {
+                    string headerLine = reader.ReadLine();
+                    string[] headers = headerLine.Split(',');
+                    DataTable dataTable = new DataTable();
+                    foreach (string header in headers)
+                    {
+                        dataTable.Columns.Add(header);
+                    }
+                    while (!reader.EndOfStream)
+                    {
+                        string dataLine = reader.ReadLine();
+                        string[] values = dataLine.Split(',');
+                        DataRow row = dataTable.NewRow();
+                        for (int i = 0; i < headers.Length; i++)
+                        {
+                            row[i] = values[i];
+                        }
+                        dataTable.Rows.Add(row);
+                    }
+
+                    // Hiển thị dữ liệu trong DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+
+            // Tìm lớp dự báo
+            DataTable dt = dataGridView1.DataSource as DataTable;
+            if (dt != null)
+            {
+                predictionClass[0].name = dt.Rows[0]["Loai_da_quy"].ToString();
+                predictionClass[0].count = 0;
+                int indexColumn = 0;
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    string name = row["Loai_da_quy"].ToString();
+                    for (int i = 0; i < totalPredictionClass; i++)
+                    {
+                        if (name.Equals(predictionClass[i].name))
+                        {
+                            predictionClass[i].count++;
+                            break;
+                        }
+                        if (i == totalPredictionClass - 1)
+                        {
+                            predictionClass[totalPredictionClass].name = name;
+                            predictionClass[totalPredictionClass].count = 0;
+                            totalPredictionClass++;
+                        }
+                    }
+                }
+
+                dataTrain[0].name = dt.Rows[0][1].ToString();
+                dataTrain[0].count = new int[totalPredictionClass];
+                dataTrain[0].column = 0;
+
+                for (int i = 1; i < dataGridView1.ColumnCount; i++)
+                {
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string item = row[i].ToString();
+                        string itemPredictionClass = row["Loai_da_quy"].ToString();
+                        int index = 0;
+                        bool store = false;
+
+                        for (int j = 0; j < totalPredictionClass; j++)
+                        {
+                            if (itemPredictionClass.Equals(predictionClass[j].name))
+                            {
+                                index = j;
+                                break;
+                            }
+                        }
+
+                        for (int j = 0; j < totalDataTrain; j++)
+                        {
+                            if (item.Equals(dataTrain[j].name) && dataTrain[j].column == i)
+                            {
+                                dataTrain[j].count[index]++;
+                                store = true;
+                                break;
+                            }
+                        }
+
+                        if (store == false)
+                        {
+                            dataTrain[totalDataTrain].name = item;
+                            dataTrain[totalDataTrain].count = new int[totalPredictionClass];
+                            dataTrain[totalDataTrain].count[index]++;
+                            dataTrain[totalDataTrain].column = i;
+                            totalDataTrain++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < totalPredictionClass; i++)
+                {
+                    predictionClass[i].p = predictionClass[i].count * 1.0 / dataGridView1.Height;
+                    richTextBox1.Text += "Name: " + predictionClass[i].name + " Count:" + predictionClass[i].count + " P: " + predictionClass[i].p + "\n";
+                }
+
+                for (int i = 0; i < totalDataTrain; i++)
+                {
+                    richTextBox1.Text += "Name: " + dataTrain[i].name + " column: " + dataTrain[i].column + "\n";
+                    dataTrain[i].p = new double[totalPredictionClass];
+                    for (int j = 0; j < totalPredictionClass; j++)
+                    {
+                        dataTrain[i].p[j] = dataTrain[i].count[j] * 1.0 / predictionClass[j].count;
+                        richTextBox1.Text += "Count " + predictionClass[j].name + ": " + dataTrain[i].count[j] + " p: " + dataTrain[i].p[j] + "\n";
+                    }
+                    richTextBox1.Text += "\n";
+                }
+
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    if (indexColumn == 0)
+                    {
+                        indexColumn++;
+                        continue;
+                    }
+                    Label label = new Label();
+                    label.Text = column.Name;
+                    TextBox textBox = new TextBox();
+
+                    // Thiết lập Text của Label là tên nhãn cho TextBox tương ứng
+                    textBox.Name = "textBox1";
+                    textBox.Width = flowLayoutPanel1.Width / 2;
+
+                    // Thêm Label và TextBox vào FlowLayoutPanel
+                    flowLayoutPanel1.Controls.Add(label);
+                    flowLayoutPanel1.Controls.Add(textBox);
+                    indexColumn++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int column = 1;
+            double maxF = 0;
+            string className = predictionClass[0].name;
+            foreach (Control control in flowLayoutPanel1.Controls)
+            {
+                if (control is TextBox)
+                {
+                    bool check = false;
+                    if (control.Text == "")
+                    {
+                        MessageBox.Show("Trường " + dataGridView1.Columns[column].Name + " Không được để trống!", "Hộp thoại nhập trường dự đoán", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    for (int j = 0; j < totalDataTrain; j++)
+                    {
+                        if (dataTrain[j].column == column && dataTrain[j].name.Equals(control.Text))
+                        {
+                            check = true;
+                            break;
+                        }
+                    }
+
+                    if (!check)
+                    {
+                        MessageBox.Show("Trường " + dataGridView1.Columns[column].Name + " Không hợp lệ!", "Hộp thoại nhập trường dự đoán", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                    column++;
+                }
+            }
+
+            column = 1;
+
+            for (int i = 0; i < totalPredictionClass; i++)
+            {
+                double maxFCon = predictionClass[i].p;
+
+                foreach (Control control in flowLayoutPanel1.Controls)
+                {
+                    if (control is TextBox)
+                    {
+                        for (int j = 1; j < totalDataTrain; j++)
+                        {
+                            if (dataTrain[j].name.Equals(control.Text))
+                            {
+                                maxFCon = maxFCon * dataTrain[j].p[i];
+                            }
+                        }
+                        column++;
+                    }
+                }
+                if (maxF < maxFCon)
+                {
+                    maxF = maxFCon;
+                    className = predictionClass[i].name;
+                }
+            }
+            lbKDD.Text = "Loại đá quý được dự đoán: " + className;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
